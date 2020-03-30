d3f7390ca29eef79beff935b26ee053d7d2fe7a4
Galang Piliang <galang.pil@gmail.com>
2020-03-30 15:41:23 +0700

    [Galang] - clear warning

--- src/assets/components/landing/HeaderNav.js
+++ src/assets/components/landing/HeaderNav.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import logo from "../../images/logo.png";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 
 export class HeaderNav extends Component {
   render() {
